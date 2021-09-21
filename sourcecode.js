10564d7931b2ceb7d8ed20edc7c8407b5e44e3a0
Samrat Mallick <samrat.mallick1@gmail.com>
2021-09-21 19:03:29 +0530

    updated

--- sourcecode.js
+++ sourcecode.js
@@ -1,7 +1,10 @@
 // 1. A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destruction the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable.
 const student = ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]];
 const [name, skills, scores] = student;
-const [, , jsScore, reactScore] = student[2];
+const jsScoreIndex = skills.findIndex((x) => x === "JS");
+const reactScoreIndex = skills.findIndex((x) => x === "React");
+const jsScore = scores[jsScoreIndex];
+const reactScore = scores[reactScoreIndex];
 console.log(name, skills, scores);
 console.log(jsScore, reactScore);
 
